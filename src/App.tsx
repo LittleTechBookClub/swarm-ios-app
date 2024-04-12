64ee1fb0a1738ad63f1327417d12de8dbd7e3562
Linden Chiu <lindentree@Lindens-MacBook-Pro.local>
2024-04-12 14:13:47 -0700

    add stack navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,31 @@ import { registerRootComponent } from 'expo';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
+
+import Contributors from './Contributors';
+
+const Stack = createNativeStackNavigator();
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Swarm Beekeeping App</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
 
 const App = () => {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Swarm Beekeeping App</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Contributors" component={Contributors} />
+      </Stack.Navigator >
     </NavigationContainer >
+
   );
 }
 
---
+++ src/Contributors.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Contributors = () => {
+
+    return (
+        <div>
+            <h1>Contributors</h1>
+            <ul>
+                <li>John Doe</li>
+                <li>Jane Doe</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Contributors;
